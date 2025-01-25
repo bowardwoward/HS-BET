e31da609eb37509a31364fea6d8b42ab1a284a8d
bowardwoward <abgao.elijah@hyperstacksinc.com>
2025-01-25 15:52:35 +0800

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('BET API Docs')
+    .setVersion('1.0')
+    .build();
+  app.setGlobalPrefix('api');
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
